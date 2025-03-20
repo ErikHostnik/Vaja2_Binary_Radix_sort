0ef243b185f0994732531eec583c2d29792066e1
erikh <erik.hostnik@gmail.com>
2025-03-20 16:04:01 +0100

    Implemented the strucutre for the algorithm

--- radix_sort.cpp
+++ radix_sort.cpp
@@ -26,3 +26,22 @@ void Izpis_Stevil(int* polje, unsigned int velikost) {
 	for (int i = 0; i<velikost; i++)
 		output << polje[i] << ' ';
 }
+
+int main(int argc, const char* argv[]) {
+	vector<int> A;
+
+	if (argc < 3) return 0;
+	if (!Branje_Stevil(A, argv[2])) return 0;
+
+	if (argv[1][0] == '0') {
+		
+
+        
+
+
+	}
+	
+	Izpis_Stevil(&A[0],A.size());
+
+	return 0;
+}
