cea1ba6bef9a8f6211bfcc90498d4950457bc22b
erikh <erik.hostnik@gmail.com>
2025-03-20 15:46:33 +0100

    Implemented printing numbers

--- radix_sort.cpp
+++ radix_sort.cpp
@@ -19,3 +19,10 @@ bool Branje_Stevil(vector<int> &vec, const char s[]) {
 	input.close();
 	return true;
 }
+
+void Izpis_Stevil(int* polje, unsigned int velikost) {
+	ofstream output("out.txt");
+
+	for (int i = 0; i<velikost; i++)
+		output << polje[i] << ' ';
+}
