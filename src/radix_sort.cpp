effe0bd283cc1096916a26c67b35fdc4c8ec0c7f
Erik Hostnik <erik.hostnik@gmail.com>
2025-04-13 19:14:01 +0200

    Basic test

--- src/radix_sort.cpp
+++ src/radix_sort.cpp
@@ -1,26 +1,26 @@
 #include <fstream>
 #include <vector>
 #include <iostream>
-using namespace std; //test test1
+using namespace std; 
 
-bool Branje_Stevil(vector<int> &vec, const char s[]) { //test test2
+bool Branje_Stevil(vector<int> &vec, const char s[]) {
 	ifstream input(s);
 	int st;
 
-	if (!input.is_open()) {   //test test3
+	if (!input.is_open()) {   
 		return false;
 	}
 
-	while (!input.eof()) { //test 5
+	while (!input.eof()) { 
 		input >> st;
 		vec.push_back(st);
-		while (isspace(input.peek())) input.get(); //test est 4
+		while (isspace(input.peek())) input.get(); 
 	}
 	input.close();
 	return true;
 }
 
-void Izpis_Stevil(int* polje, unsigned int velikost) {  //test test 5
+void Izpis_Stevil(int* polje, unsigned int velikost) {  
 	ofstream output("out.txt");
 
 	for (int i = 0; i<velikost; i++)
